dae06153bdebe737388d9dcfed9b9a826658290f
Kimberly <54862957+kimberly-amador@users.noreply.github.com>
2024-08-25 15:11:51 -0600

    Update utils.py

--- model/utils.py
+++ model/utils.py
@@ -78,46 +78,6 @@ class DECODER:
         return output
 
 
-class SelfAttention:
-    @staticmethod
-    def build(inputTensor, n_layers, n_heads, dropout, positional_encoding=True):
-        # Define embed_dim
-        embed_dim = inputTensor.shape[2]
-
-        # Positional embedding layer (optional)
-        if positional_encoding:
-            sequence_length = inputTensor.shape[1]  # inputs are of shape: (batch_size, n_timepoints, embed_dim)
-            positions = tf.range(start=0, limit=sequence_length, delta=1)
-            embedded_positions = Embedding(input_dim=sequence_length, output_dim=embed_dim)(positions)
-            inputTensor += embedded_positions
-
-        # Multi-head self-attention layer
-        for _ in range(n_layers):
-            attention_output, attention_scores = MultiHeadAttention(num_heads=n_heads, key_dim=embed_dim, dropout=dropout)(inputTensor, inputTensor, return_attention_scores=True)
-            inputTensor = LayerNormalization()(inputTensor + attention_output)  # res connection provides a direct path for the gradient, while the norm maintains a reasonable scale for outputs
-
-        return inputTensor
-
-
-class MultimodalFusion:
-    @staticmethod
-    def build(image_embed, clinical_embed, n_heads=8, dropout=0.2):
-        # Define embed_dim
-        embed_dim = image_embed.shape[2]
-
-        # Co-attention: Query - Imaging; Key & Value - Metadata
-        A_attention_output, A_attention_scores = MultiHeadAttention(num_heads=n_heads, key_dim=embed_dim, dropout=dropout)(image_embed, clinical_embed, return_attention_scores=True)
-        A_proj_input = LayerNormalization()(image_embed + A_attention_output)
-        A_proj_output = Sequential([Dense(embed_dim, activation=tf.nn.gelu), Dense(embed_dim), ])(A_proj_input)
-        A_x = LayerNormalization()(A_proj_input + A_proj_output)  # shape (None, 32, 2704)
-
-        # Reduce output sequence through pooling layer
-        A_x = GlobalMaxPooling1D()(A_x)  # shape (None, 2704)
-        A_output = Dropout(dropout)(A_x)
-
-        return A_output
-
-
 class TRANSFORMER:
     @staticmethod
     def build(inputTensor, n_layers, n_heads, dropout, positional_encoding=True):
